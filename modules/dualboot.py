b8e3380482ed9cc0aed951bb126a438f00ac21a5
NEOAPPS <158327205+neoapps-dev@users.noreply.github.com>
2025-08-18 08:16:39 -0400

    feat: switch from dd to rsync (#21)

--- modules/dualboot.py
+++ modules/dualboot.py
@@ -169,7 +169,18 @@ LABEL=home_ab /home  ext4  defaults,noatime 0 2
     print("Unmounting slot 'a' partitions before copy...")
     run_command(f"umount -R {mount_dir}")
     print("Copying system to slot 'b'...")
-    run_command(f"bash -c \"pv \\\"{part3}\\\" | dd oflag=sync of={part4} bs=16M\"")
+    source_mount_point = "/mnt/obsidian_source_a"
+    target_mount_point = "/mnt/obsidian_target_b"
+    run_command(f"mkdir -p {source_mount_point} {target_mount_point}")
+    try:
+        run_command(f"mount {part3} {source_mount_point}")
+        run_command(f"mount {part4} {target_mount_point}")
+        run_command(f"rsync -aHAX --inplace --delete --info=progress2 {source_mount_point}/ {target_mount_point}/")
+    finally:
+        run_command(f"umount {source_mount_point}", check=False)
+        run_command(f"umount {target_mount_point}", check=False)
+        run_command(f"rm -r {source_mount_point} {target_mount_point}", check=False)
+
     run_command(f"e2label {part4} root_b")
     print("Correcting fstab for slot 'b'...")
     mount_b_dir = "/mnt/obsidian_install_b"
