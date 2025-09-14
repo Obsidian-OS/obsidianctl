c49ed62274125c93777512f409082c651bb3a060
NEOAPPS <158327205+neoapps-dev@users.noreply.github.com>
2025-09-14 21:05:31 +0300

    feat(modules): new systemd-boot-config module (#31)
    * feat: enable and disable
    
    * feat: switch-kernel

--- modules/utils.py
+++ modules/utils.py
@@ -4,11 +4,20 @@ import sys
 import os
 import subprocess
 import re
+import pwd
+import tempfile
 
-def lordo(label, disk=None):  # LORDO = LABEL On Root Disk Only, returns /dev/disk/by-uuid/UUID
-    root_part = subprocess.check_output(["findmnt", "-no", "SOURCE", "/"], text=True).strip()
+
+def lordo(
+    label, disk=None
+):  # LORDO = LABEL On Root Disk Only, returns /dev/disk/by-uuid/UUID
+    root_part = subprocess.check_output(
+        ["findmnt", "-no", "SOURCE", "/"], text=True
+    ).strip()
     if disk is None:
-        disk_name = subprocess.check_output(['lsblk', '-no', 'PKNAME', root_part], text=True).strip()
+        disk_name = subprocess.check_output(
+            ["lsblk", "-no", "PKNAME", root_part], text=True
+        ).strip()
         disk = f"/dev/{disk_name}"
     parts = subprocess.check_output(
         ["lsblk", "-o", "NAME,LABEL,UUID", "-l", disk], text=True
@@ -23,16 +32,20 @@ def lordo(label, disk=None):  # LORDO = LABEL On Root Disk Only, returns /dev/di
 
     return None
 
+
 def check_dependencies(commands):
     commands.extend(["curl", "tar", "mksquashfs", "unsquashfs"])
     for command in commands:
         if not shutil.which(command):
             print(f"Error: Required command '{command}' not found.", file=sys.stderr)
             sys.exit(1)
+
+
 def checkroot():
     if os.geteuid() != 0:
-      print("This script must be run as root.", file=sys.stderr)
-      sys.exit(1)
+        print("This script must be run as root.", file=sys.stderr)
+        sys.exit(1)
+
 
 def run_command(command, **kwargs):
     kwargs.setdefault("text", True)
@@ -92,3 +105,17 @@ def get_current_slot():
     except (subprocess.CalledProcessError, FileNotFoundError):
         pass
     return "unknown"
+
+def get_user_home_dir():
+    if 'SUDO_USER' in os.environ:
+        try:
+            user = os.environ['SUDO_USER']
+            return pwd.getpwnam(user).pw_dir
+        except KeyError:
+            pass
+    return os.path.expanduser("~")
+
+def get_primary_disk_device():
+    root_part = run_command("findmnt -no SOURCE /", capture_output=True).stdout.strip()
+    disk_name = run_command(f"lsblk -no PKNAME {root_part}", capture_output=True).stdout.strip()
+    return f"/dev/{disk_name}"
