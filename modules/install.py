7a3569c588b4f082e29c3ce2d5ff3dce6aa32363
/home/neo <158327205+neoapps-dev@users.noreply.github.com>
2025-11-14 16:50:25 +0300

    fix(modules(install)): mkdir (#42)
    * fix(modules(install)): mkdir
    
    * fix(modules(dualboot)): mkdir
    
    * Change grub directory creation to /efi/grub/
    
    * Fix grub installation commands and directory creation

--- modules/install.py
+++ modules/install.py
@@ -267,10 +267,12 @@ WantedBy=getty.target
         if args.use_grub2:
             run_command(f"arch-chroot {mount_dir} grub2-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=ObsidianOSslotA")
             run_command(f"arch-chroot {mount_dir} sed -i 's|^#*GRUB_DISABLE_OS_PROBER=.*|GRUB_DISABLE_OS_PROBER=false|' /etc/default/grub")
+            run_command(f"mkdir {mount_dir}/efi/grub/ -p")
             run_command(f"arch-chroot {mount_dir} grub2-mkconfig -o /efi/grub/grub.cfg")
         else:
             run_command(f"arch-chroot {mount_dir} grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=ObsidianOSslotA")
             run_command(f"arch-chroot {mount_dir} sed -i 's|^#*GRUB_DISABLE_OS_PROBER=.*|GRUB_DISABLE_OS_PROBER=false|' /etc/default/grub")
+            run_command(f"mkdir {mount_dir}/efi/grub/ -p")
             run_command(f"arch-chroot {mount_dir} grub-mkconfig -o /efi/grub/grub.cfg")
         run_command(f"umount -R {mount_dir}")
         mount_commands = [
@@ -284,9 +286,11 @@ WantedBy=getty.target
             run_command(cmd)
         if args.use_grub2:
             run_command(f"arch-chroot {mount_dir} grub2-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=ObsidianOSslotA")
+            run_command(f"mkdir {mount_dir}/efi/grub/ -p")
             run_command(f"arch-chroot {mount_dir} grub2-mkconfig -o /efi/grub/grub.cfg")
         else:
             run_command(f"arch-chroot {mount_dir} grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=ObsidianOSslotA")
+            run_command(f"mkdir {mount_dir}/efi/grub/ -p")
             run_command(f"arch-chroot {mount_dir} grub-mkconfig -o /efi/grub/grub.cfg")
         run_command(f"mkdir {mount_dir}/boot/grub/ -p")
         run_command(f"arch-chroot {mount_dir} ln -sf ln -sf /efi/grub/grub.cfg /boot/grub/grub.cfg")
